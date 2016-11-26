62c9f06b6611a99793ba4b71d34cbfb36ea5c413
gregend <drizzt52@gmail.com>
2016-11-26 09:05:32 +0100

    Django

---
+++ SimpleEncryption1.py
@@ -0,0 +1,13 @@
+def decrypt(encrypted_text, n):
+    pass 
+
+
+def encrypt(text, n):
+    for i in range(0,len(text)):
+        print(3)
+        
+
+encrypt("text",2)
+for id in array:
+    print()
+
